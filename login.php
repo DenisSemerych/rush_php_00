8436b6169120ba52760260f8224c27b652da11d0
Denis SEMERYCH <dsemeryc@e2r10p7.unit.ua>
2019-01-19 20:20:46 +0200

    changed

--- login.php
+++ login.php
@@ -1,43 +1,32 @@
 <?php
-$users = unserialize(file_get_contents("../private/users"));
-$newusers = array();
+$users = unserialize(file_get_contents("./private/users"));
 if ($_POST['login'] && $_POST['passwd'] && $_POST['submit'] == 'Create')
 {
-	foreach ($users as $user)
-	{
-		if ($user['login'] == $_POST['login'])
-		{
+	foreach ($users as $user) {
+		if ($user['login'] == $_POST['login']) {
 			exit("This login alredy exists, try another one");
 		}
-		$newusers[] = $user;
 	}
+    if ($_POST['rpswd'] == $_POST['newpswd']) {
+        $users[] = array('login' => $_POST['newlogin'], 'passwd' => hash('whirlpool', $_POST['newpswd']));
+        if (file_put_contents("./private/users", serialize($users))) {
+            $_SESSION['logged_in_user'] = $users['login'];
+            header("Location: index.php");
+        }
+    }
 }
 else if ($_POST['login'] && $_POST['passwd'] && $_POST['submit'] == 'Login')
 {
-	foreach ($users as $user)
-	{
-		if ($user['login'] == $_POST['login'])
-		{
-			if ($user['passwd'] == hash('whirlpool', $_POST['passwd']))
-			{
+	foreach ($users as $user) {
+		if ($user['login'] == $_POST['login']) {
+			if ($user['passwd'] == hash('whirlpool', $_POST['passwd'])) {
 				$_SESSION['logged_in_user'] = $user['login'];
 				header("Location: index.php");
-			}
-			else
-			{
+			} else {
 				exit("Wrong password\n");
 			}
 		}
 	}
 	exit("Login doesn`t exists");
-}
-if ($_POST['rpswd'] == $_POST['newpswd'])
-{
-    $newusers[] = array('login' => $_POST['newlogin'], 'passwd' => hash('whirlpool', $_POST['newpswd']));
-    if (file_put_contents("../private/users", serialize($newusers)))
-	{
-        $_SESSION['logged_in_user'] = $user['login'];
-        header("Location: index.php");
-	}
 }
 ?>
